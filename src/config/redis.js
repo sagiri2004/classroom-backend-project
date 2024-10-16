5ba8eb8bd9dcee603949fd01f47b98500fccbc98
Sagiri <thang.nh225763@sis.hust.edu.vn>
2024-10-17 00:11:10 +0700

    deploy mysql

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 const Redis = require('ioredis');
 
 //redis://red-cs7uei88fa8c73cn48kg:6379
-const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis("redis://red-cs7uei88fa8c73cn48kg:6379");
 
 module.exports = redis;
